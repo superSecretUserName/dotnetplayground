0593ad30c502d4eabf696f4d02a987a1353043e1
supersecretusername <ds>
2025-02-06 16:08:43 -0500

    wired up ms unit testing scaffolding and solution folder.

---
+++ MyDotnetWebApp.Tests/Test1.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MyDotnetWebApp.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
+
+internal class TestClassAttribute : Attribute
+{
+}
+
+internal class TestMethodAttribute : Attribute
+{
+}
